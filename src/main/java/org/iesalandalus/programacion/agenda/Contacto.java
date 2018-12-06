17bbb740af9d1762903697aa1837855eacc01043
David <David@Davi-PC>
2018-12-06 20:21:42 +0100

    Añadimos la clase "Contacto" al commit.

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -2,4 +2,11 @@ package org.iesalandalus.programacion.agenda;
 
 public class Contacto {
 
+	private static final String ER_TELEFONO;
+	private static final String ER_CORREO;
+	private String nombre;
+	private String telefono;
+	private String correo;
+	
+	
 }
