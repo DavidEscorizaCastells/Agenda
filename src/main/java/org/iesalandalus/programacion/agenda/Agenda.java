3bf94ece1903c13d1b8d58ac73ede97662bc7f89
David <David@Davi-PC>
2018-12-08 18:42:59 +0100

    Creamos constructor por defecto y los Getters.

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -5,5 +5,21 @@ public class Agenda {
 	private static final int MAX_CONTACTOS=20;
 	int numContactos;
 	Contacto[] contactos;
-
+	
+	public Agenda () {
+		numContactos=0;
+		contactos=new Contacto[MAX_CONTACTOS];
+	}
+	
+	public Contacto[] getContactos() {
+		Contacto[] copiaContactos = new Contacto[MAX_CONTACTOS];
+		for (int i=0; i<copiaContactos.length; i++) {
+			copiaContactos[i]=contactos[i];
+		}		
+		return copiaContactos;
+	}
+	
+	public int getNumContactos() {
+		return numContactos;
+	}
 }
