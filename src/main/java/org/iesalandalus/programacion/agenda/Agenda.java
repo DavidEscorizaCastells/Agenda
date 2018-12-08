150edd264221c952c34c824deefea565dbcb3edc
David <David@Davi-PC>
2018-12-08 19:05:08 +0100

    Creamos los métodos añadir, buscarPrimerIndiceComprobandoExistencia y indiceNoSuperaTamaño.

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.agenda;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Agenda {
 	
 	private static final int MAX_CONTACTOS=20;
@@ -21,5 +23,31 @@ public class Agenda {
 	
 	public int getNumContactos() {
 		return numContactos;
+	}
+	
+	public void añadir(Contacto contacto) throws OperationNotSupportedException {
+		if (buscarPrimerIndiceComprobandoExistencia(contacto)==-1) {
+			if (indiceNoSuperaTamaño(numContactos)) {
+				contactos[numContactos]=contacto;
+				numContactos+=1;
+			}	else throw new OperationNotSupportedException ("La agenda está completa.");
+		} throw new OperationNotSupportedException ("El contacto "+contacto.toString()+" ya existe.");
+		
+	}
+	
+	private int buscarPrimerIndiceComprobandoExistencia(Contacto contacto) {;
+		for (int i=0;i<contactos.length;i++) {
+			if (contactos[i].equals(contacto)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private boolean indiceNoSuperaTamaño(int indice) {
+		if (numContactos>MAX_CONTACTOS) {
+			return false;
+		}
+		return true;
 	}
 }
